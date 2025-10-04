367ee2e466eee4a3582628557ab8d37811df67c7
dannypas00 <github@dannypas.nl>
2025-10-04 21:08:49 +0200

    Copy over initial backend functionality for job progress

--- routes/authenticated/webapi.php
+++ routes/authenticated/webapi.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Jobs\TestJob;
+
+Route::post('test-job', static function (Request $request) {
+    $job = new TestJob($request->);
+    dispatch($job);
+
+    return $job->getIdentifier();
+});
